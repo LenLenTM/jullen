620dcc51ec83d18866ad927eaea116b8d99a55ed
LenLenTM <91597420+LenLenTM@users.noreply.github.com>
2023-08-17 09:23:37 +0200

    start

---
+++ api/routes.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+const requestController = require('requestController');
+const routes = Router();
+
+routes.get
---
+++ app.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const nodeSession = require('express-session');
+const fs = require('fs');
+const https = require('https');
+
+const app = express();
+const hostname = '10.10.10.100';
+const PORT = 443;
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+app.use(nodeSession({
+    secret:"le262na18",
+    saveUninitialized: true,
+    cookie: {maxAge: oneDay},
+    resave: false
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'pages')));
+app.use(cookieParser());
+
+app.get("/", function (req, res){
+    res.sendFile(__dirname + "/pages/index.html");
+});
+
+const options = {
+    key: fs.readFileSync("/home/Lena/jullen/certs/_.jullen.at_private_key.key"),
+    cert: fs.readFileSync("/home/Lena/jullen/certs/jullen.at_ssl_certificate.cer"),
+};
+
+https.createServer(options, app).listen(PORT, hostname, () => console.log("Server started"));
