c5a19ab01426484ea6dd45f039ad805d2f01a4ae
LenLenTM <91597420+LenLenTM@users.noreply.github.com>
2023-08-23 20:28:42 +0200

    first styling

--- api/routes.js
+++ api/routes.js
@@ -6,8 +6,4 @@ const authSession = require('./authSession.js');
 routes.post('/login', requestController.login);
 routes.post('/register', requestController.register);
 
-routes.get('/loggedTest', authSession, (req, res, next) => {
-    res.redirected('/allGood');
-});
-
 module.exports = routes;
--- api/standardRoutes.js
+++ api/standardRoutes.js
@@ -3,10 +3,10 @@ const standardRoutes = Router();
 const authSession = require('./authSession.js');
 const path = require('path');
 
-standardRoutes.get('/loggedTest', authSession, (req, res, next) => {
+/*standardRoutes.get('/loggedTest', authSession, (req, res, next) => {
     console.log("standardRoutes");
     res.redirect('/allGood');
-});
+});*/
 
 standardRoutes.get('', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
